30d0d2c68ae82b5d270d8993ce6cbd71a464955c
h65rdl0ckl3z <bank427ingza@gmail.com>
2024-04-24 01:10:52 +0700

    bkl commit

--- backend/config/main.php
+++ backend/config/main.php
@@ -46,19 +46,6 @@ return [
             ],
         ],
         */
-        'urlManager' => [
-            'class' => 'yii\web\UrlManager',
-            // Disable index.php
-            'showScriptName' => false,
-            // Disable r= routes
-            'enablePrettyUrl' => false,
-            'rules' => [
-                '<controller:\w+>/<id:\d+>' => '<controller>/view',
-                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
-                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
-                'module/<module:\w+>/<controller:\w+>/<action:\w+>' => '<module>/<controller>/<action>',
-            ],
-        ],
     ],
     'params' => $params,
 ];
